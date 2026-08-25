7ece224c24efc1c6a61578163b2255aaa092f252
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-25 21:25:22 +0300

    Export optimization and sensitivity APIs

--- src/multi_echelon_inventory/__init__.py
+++ src/multi_echelon_inventory/__init__.py
@@ -2,6 +2,8 @@ from .models import InventoryNode, SimulationConfig
 from .policies import PolicyType
 from .simulation import MultiEchelonSystem, SimulationResult
 from .monte_carlo import MonteCarloResult, compare_policies
+from .optimization import OptimizationResult, optimize_service_level
+from .sensitivity import SensitivityResult, sensitivity_analysis
 
 __all__ = [
     "InventoryNode",
@@ -11,4 +13,8 @@ __all__ = [
     "SimulationResult",
     "MonteCarloResult",
     "compare_policies",
+    "OptimizationResult",
+    "optimize_service_level",
+    "SensitivityResult",
+    "sensitivity_analysis",
 ]
